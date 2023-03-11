ba1f84a1da1b833c9ff6ba376a4786fdf65bf6a8
Nikita <grigoryantsnm@gmail.com>
2023-03-11 20:11:34 +0300

    first commit

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int start = 100; // начальный баланс
+        int replenishment = 1100; // сумма пополнения
+        int bonus;
+
+        if (replenishment > 1000) {
+            bonus = replenishment / 100;
+        } else { bonus = 0;
+        }
+        System.out.println("счет клиента:" + (start + replenishment + bonus) + "рублей" );
+    }
+}
